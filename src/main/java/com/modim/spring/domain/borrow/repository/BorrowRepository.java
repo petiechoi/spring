2f116e2bdf0158ca23e078f190291a42332e45b6
petiechoi <128676659+petiechoi@users.noreply.github.com>
2023-07-05 10:53:43 +0900

    Revert "Hotfix/postMethod"

--- src/main/java/com/modim/spring/domain/borrow/repository/BorrowRepository.java
+++
@@ -1,9 +0,0 @@
-package com.modim.spring.domain.borrow.repository;
-
-import com.modim.spring.domain.borrow.model.Borrow;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BorrowRepository extends JpaRepository<Borrow, Long> {
-}
